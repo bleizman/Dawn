a312f4912e9361e4720442214cbf10547801da59
Colter Smith <crsmith@princeton.edu>
2015-04-13 23:35:58 -0400

    Added preferences files

---
+++ Dawn/DawnPreferences.h
@@ -0,0 +1,24 @@
+//
+//  DawnPreferences.h
+//  Dawn
+//
+//  Created by Colter Smith on 4/13/15.
+//  Copyright (c) 2015 Dawnteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DawnPreferences : NSObject
+
+@property NSString *name;
+
+@property BOOL *weatheron;
+@property NSInteger *zipcode;
+
+@property BOOL *newson;
+@property NSSet *newssites;
+
+- (id)initWithName:(NSString*) name;
+
+
+@end
