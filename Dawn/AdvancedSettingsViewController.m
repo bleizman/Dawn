3d3e6b5da6d3d504a68660af6a2509312eccf2c5
Ben Leizman <bleizman@princeton.edu>
2015-04-16 20:28:44 -0400

    StoryBoard edits- Don't think this is right so don't pull just yet

---
+++ Dawn/AdvancedSettingsViewController.m
@@ -0,0 +1,26 @@
+//
+//  AdvancedSettingsViewController.m
+//  Dawn
+//
+//  Created by Ben Leizman on 4/13/15.
+//  Copyright (c) 2015 Dawnteam. All rights reserved.
+//
+
+#import "AdvancedSettingsViewController.h"
+
+@interface AdvancedSettingsViewController ()
+
+@end
+
+@implementation AdvancedSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue {
+    
+}
+
+@end
--- Dawn/AdvancedViewController.h
+++ Dawn/AdvancedViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SecondViewController : UIViewController
+@interface AdvancedViewController : UIViewController
 
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
 
 @end
 
