2e80afe58183739dc1e811e97e98846b6d42a1cb
Ben Leizman <bleizman@princeton.edu>
2015-04-16 22:07:49 -0400

    Created rest of view controller and tab controller files. Didn't make navigation controller files, but don't think we need them

---
+++ Dawn/MyAlarmsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyAlarmsTableViewController.h
+//  Dawn
+//
+//  Created by Ben Leizman on 4/16/15.
+//  Copyright (c) 2015 Dawnteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAlarmsTableViewController : UITableViewController
+
+@end
---
+++ Dawn/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Dawn
+//
+//  Created by Ben Leizman on 4/16/15.
+//  Copyright (c) 2015 Dawnteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
