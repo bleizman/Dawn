c6ddfe426463352cb6720cd842085cbf390b08b5
Jack O'Brien <jackobrien@nat-oitwireless-inside-vapornet100-a-8008.Princeton.EDU>
2015-05-05 17:08:16 -0400

    minor changes

--- Dawn/LoginViewController.m
+++ Dawn/LoginViewController.m
@@ -12,7 +12,6 @@
 #import <Parse/Parse.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
-#import <FBSDKShareKit/FBSDKShareKit.h>
 #import <FBSDKCoreKit/FBSDKProfile.h>
 #import <ParseUI/ParseUI.h>
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
